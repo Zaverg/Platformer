43b2f45e377e0867c697f59d41034b2dd6c23981
Zaverg <i2020kdv@gmail.com>
2025-06-21 11:02:49 +0300

    .

--- Assets/Scripts/Characters/Player/Mover.cs
+++ Assets/Scripts/Characters/Player/Mover.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
-    [SerializeField] private float _climbForce = 10f;
 
     private Rigidbody2D _rigidbody;
 
@@ -15,7 +14,6 @@ public class Mover : MonoBehaviour
 
     public void Move(Vector2 direction)
     {
-        _rigidbody.AddForce(direction * _speed * _climbForce);
-        _rigidbody.velocity = Vector2.ClampMagnitude(_rigidbody.velocity, _speed);
+        _rigidbody.velocity = new Vector2(direction.x * _speed, direction.y);
     }
 }
